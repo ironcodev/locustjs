a323650fa70ae0e0bb5e8bf16df06d0db7658dc8
Mansoor Omrani <mansoor.omrani@gmail.com>
2020-04-06 08:19:49 +0430

    Fixed bug: jQuery not found

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-const Version = '1.1.0';
+const Version = '1.0.3';
 
 const isEmpty       = (x) => x == null || (typeof x == 'string' && x.trim() == '');
 const isSomeString  = (x) => typeof x == 'string' && x.trim() != '';
 const isSomeObject  = (x) => typeof x == 'object' && x != null;
 const isFunction    = (x) => typeof x == 'function' && typeof x.nodeType !== 'number';
-const isNumeric     = (x) => _jQuery.isNumeric(x);
+const isNumeric     = (x) => (['number', 'string'].indexOf(typeof x) >= 0 && !isNaN(x - parseFloat(x));	// borowwed from jQuery
 const isPrimitive   = (x) => !isEmpty(x) && !isFunction(x) && !isSomeObject(x) && !Array.isArray(x);
+const isArray		= Array.isArray(x);
 
 class BaseEnum {
     constructor(values, name) {
         this.name = name;
 
-        if (_jQuery.isArray(values)) {
+        if (isArray(values)) {
             for (let i; i < values.length; i++) {
                 const value = values[i];
 
@@ -38,7 +39,7 @@ class BaseEnum {
         let result = [];
 
         for (let key of Object.keys(this)) {
-            if (typeof key == 'string' && isPrimitive(this[key]) && !_jQuery.isNumeric(key) && key != 'name') {
+            if (typeof key == 'string' && isPrimitive(this[key]) && !isNumeric(key) && key != 'name') {
                 result.push({ name: key, value: this[key] })
             }
         }
@@ -57,14 +58,14 @@ const Enum = {
         let result = false;
 
         if (isSomeObject(enumType) && isPrimitive(value1) && isPrimitive(value2)) {
-            if (_jQuery.isNumeric(value1)) {
-                if (_jQuery.isNumeric(value2)) {
+            if (isNumeric(value1)) {
+                if (isNumeric(value2)) {
                     result = value1 == value2 && enumType[value1] != undefined;
                 } else {
                     result = value1 == enumType[value2];
                 }
             } else {
-                if (_jQuery.isNumeric(value2)) {
+                if (isNumeric(value2)) {
                     result = value1 == enumType[value2];
                 } else {
                     result = value1 == value2 && enumType[value1] != undefined;
